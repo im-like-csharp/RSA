53086d3a2de7e20cd180e36d59fbf22660bee0f3
im-like-csharp <andre-lomov25@mail.ru>
2019-12-04 19:06:20 +0300

    first

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p_text = new System.Windows.Forms.TextBox();
+            this.q_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.n_text = new System.Windows.Forms.TextBox();
+            this.d_text = new System.Windows.Forms.TextBox();
+            this.Generate = new System.Windows.Forms.Button();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.IN = new System.Windows.Forms.RichTextBox();
+            this.OUT = new System.Windows.Forms.RichTextBox();
+            this.Crypted = new System.Windows.Forms.RichTextBox();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.e_text = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bits = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // p_text
+            // 
+            this.p_text.Location = new System.Drawing.Point(43, 54);
+            this.p_text.Name = "p_text";
+            this.p_text.ReadOnly = true;
+            this.p_text.Size = new System.Drawing.Size(297, 20);
+            this.p_text.TabIndex = 0;
+            this.p_text.TextChanged += new System.EventHandler(this.p_text_TextChanged);
+            // 
+            // q_text
+            // 
+            this.q_text.Location = new System.Drawing.Point(43, 80);
+            this.q_text.Name = "q_text";
+            this.q_text.ReadOnly = true;
+            this.q_text.Size = new System.Drawing.Size(297, 20);
+            this.q_text.TabIndex = 1;
+            this.q_text.TextChanged += new System.EventHandler(this.q_text_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "p";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "q";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(15, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "d";
+            // 
+            // n_text
+            // 
+            this.n_text.Location = new System.Drawing.Point(43, 395);
+            this.n_text.Name = "n_text";
+            this.n_text.ReadOnly = true;
+            this.n_text.Size = new System.Drawing.Size(297, 20);
+            this.n_text.TabIndex = 5;
+            this.n_text.TextChanged += new System.EventHandler(this.n_text_TextChanged);
+            // 
+            // d_text
+            // 
+            this.d_text.Location = new System.Drawing.Point(43, 369);
+            this.d_text.Name = "d_text";
+            this.d_text.ReadOnly = true;
+            this.d_text.Size = new System.Drawing.Size(297, 20);
+            this.d_text.TabIndex = 4;
+            this.d_text.TextChanged += new System.EventHandler(this.d_text_TextChanged);
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(43, 132);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(105, 23);
+            this.Generate.TabIndex = 8;
+            this.Generate.Text = "Генерировать";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(43, 421);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(105, 23);
+            this.Decrypt.TabIndex = 9;
+            this.Decrypt.Text = "Расшифровать";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // IN
+            // 
+            this.IN.Location = new System.Drawing.Point(366, 30);
+            this.IN.Name = "IN";
+            this.IN.Size = new System.Drawing.Size(341, 149);
+            this.IN.TabIndex = 10;
+            this.IN.Text = "";
+            // 
+            // OUT
+            // 
+            this.OUT.Location = new System.Drawing.Point(366, 338);
+            this.OUT.Name = "OUT";
+            this.OUT.ReadOnly = true;
+            this.OUT.Size = new System.Drawing.Size(341, 149);
+            this.OUT.TabIndex = 11;
+            this.OUT.Text = "";
+            // 
+            // Crypted
+            // 
+            this.Crypted.Location = new System.Drawing.Point(19, 183);
+            this.Crypted.Name = "Crypted";
+            this.Crypted.Size = new System.Drawing.Size(688, 149);
+            this.Crypted.TabIndex = 12;
+            this.Crypted.Text = "";
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(235, 132);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(105, 23);
+            this.Encrypt.TabIndex = 13;
+            this.Encrypt.Text = "Зашифровать";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            this.Encrypt.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(265, 156);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 14;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // e_text
+            // 
+            this.e_text.Location = new System.Drawing.Point(43, 106);
+            this.e_text.Name = "e_text";
+            this.e_text.ReadOnly = true;
+            this.e_text.Size = new System.Drawing.Size(297, 20);
+            this.e_text.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(15, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 24);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "e";
+            // 
+            // bits
+            // 
+            this.bits.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bits.Location = new System.Drawing.Point(43, 26);
+            this.bits.MaxLength = 4;
+            this.bits.Name = "bits";
+            this.bits.Size = new System.Drawing.Size(52, 22);
+            this.bits.TabIndex = 17;
+            this.bits.Text = "512";
+            this.bits.TextChanged += new System.EventHandler(this.bits_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(0, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 24);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Bits";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 499);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.bits);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.e_text);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Encrypt);
+            this.Controls.Add(this.Crypted);
+            this.Controls.Add(this.OUT);
+            this.Controls.Add(this.IN);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.n_text);
+            this.Controls.Add(this.d_text);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.q_text);
+            this.Controls.Add(this.p_text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox p_text;
+        private System.Windows.Forms.TextBox q_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox n_text;
+        private System.Windows.Forms.TextBox d_text;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.RichTextBox IN;
+        private System.Windows.Forms.RichTextBox OUT;
+        private System.Windows.Forms.RichTextBox Crypted;
+        private System.Windows.Forms.Button Encrypt;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox e_text;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox bits;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //string r1 = "972346137165803391741260008403144412592226901362682364547049471060890442818028662684991378613392752649526029617987887831986881792545318603515625",
+            //       d1 = "22983739567277988919042289396428710786145324792192232804170083110737035787583849273823365929873717251150764764138805660660379086529618183783224083851254755909296311355783335836143389327521201376549033723148092483179600710950306613747200493079222995914375542032098763712401202494234918921944013939247333847767",
+            //       n1 = "73978911732176026833167368994754912842905264174868749338422455012684833941285514850118959086781027402141524084571780720250595184767208529052252519896226263393823641657376749316226639741705539840030421180292436487353552514212712139817343632730311183906689281114374406278900745155401497285690088598371426402521";
+            //BigInteger r11 = BigInteger.Parse(r1),
+            //           d11 = BigInteger.Parse(d1),
+            //           n11 = BigInteger.Parse(n1);
+            //Crypted.Text = Convert.ToString(BigInteger.ModPow(r11, d11, n11));
+
+            //int[] bit = tenintwo('0');
+            //for (int i = 0; i < 9; i++)
+            //    Crypted.Text += Convert.ToString(bit[i]);
+        }
+
+        string num = "0123456789",
+               abc = "0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        BigInteger p, q, n, d, Fi;
+        //int E;
+        BigInteger E;
+        int num_bits = 512;
+
+        private void TxtChanged(object sender, EventArgs e)
+        {
+            TextBox t = new TextBox();
+            t = (TextBox)(sender as Control);
+            if (t.TextLength != 0)
+            {
+                int cur = t.SelectionStart;
+                if (t.Text[0] == '0')
+                {
+                    StringBuilder str = new StringBuilder(t.Text);
+                    str.Remove(0, 1);
+                    t.Clear();
+                    t.Text += str;
+                }
+                for (int i = 0; i < t.TextLength; i++)
+                    if (num.IndexOf(t.Text[i]) == -1)
+                    {
+                        StringBuilder str = new StringBuilder(t.Text);
+                        str.Remove(i, 1);
+                        t.Clear();
+                        t.Text += str;
+                        t.SelectionStart = cur - 1;
+                    }
+            }
+        }
+
+        private void p_text_TextChanged(object sender, EventArgs e)
+        {
+            TxtChanged(sender, e);
+        }
+
+        private void q_text_TextChanged(object sender, EventArgs e)
+        {
+            TxtChanged(sender, e);
+        }
+
+        private void d_text_TextChanged(object sender, EventArgs e)
+        {
+            TxtChanged(sender, e);
+        }
+
+        private void n_text_TextChanged(object sender, EventArgs e)
+        {
+            TxtChanged(sender, e);
+        }
+
+        private void bits_TextChanged(object sender, EventArgs e)
+        {
+            TxtChanged(sender, e);
+            num_bits = int.Parse(bits.Text);
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            p = Gen_512();
+            while (!MillerRabin(p))
+                p = Gen_512();
+            //p = 3557;
+            p_text.Text = Convert.ToString(p);
+
+            q = Gen_512();
+            while (!MillerRabin(q) || q == p)
+                q = Gen_512();
+            //q = 2579;
+            q_text.Text = Convert.ToString(q);
+
+            n = p * q;
+            n_text.Text = Convert.ToString(n);
+
+            Fi = (p - 1) * (q - 1);
+
+            BigInteger n_copy = n;
+            E = n / 3;
+            //while (n_copy > 0)
+            //{
+            //    E++;
+            //    n_copy /= 10;
+            //}
+            //E /= 3;
+            while (p == E || q == E || Fi % E == 0 || !MillerRabin(E)) E++;
+            e_text.Text = Convert.ToString(E);
+
+            BigInteger x, y;
+            d = nod(Fi, E, out x, out y);
+            if (y < 0) d = y + Fi;
+            else d = y;
+            d_text.Text = Convert.ToString(d);
+        }
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            if (IN.TextLength != 0)
+            {
+                Crypted.Text = "";
+                for (int i = 0; i < IN.TextLength; i++)
+                {
+                    Crypted.Text += Convert.ToString(BigInteger.ModPow(abc.IndexOf(IN.Text[i]) + 13, E, n)) + ' ';
+                }
+            }
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            if (Crypted.TextLength != 0)
+            {
+                OUT.Text = "";
+                string s = Crypted.Text;
+                while (s.Length > 0)
+                {
+                    int j = s.IndexOf(' ');
+                    string s2 = s.Substring(0, j);
+                    s = s.Remove(0, j + 1);
+                    BigInteger r = BigInteger.Parse(s2);
+                    int c_num;
+                    try
+                    {
+                        c_num = int.Parse(Convert.ToString(BigInteger.ModPow(r, d, n) - 13));
+                    }
+                    catch
+                    {
+                        Random rnd = new Random();
+                        c_num = rnd.Next(130);
+                    }
+                    char c = abc[c_num];
+                    OUT.Text += c;
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            IN.Clear();
+            OUT.Clear();
+            Crypted.Clear();
+        }
+
+        private BigInteger Gen_512()
+        {
+            Random rnd = new Random();
+            BigInteger num = 0;
+            for (int i = 0; i < num_bits; i++)
+            {
+                if (rnd.Next(2) != 0)
+                    num += BigInteger.Pow(2, i);
+            }
+            return num;
+        }
+
+        private bool MillerRabin(BigInteger n)
+        {
+            if (n == 2 || n == 3)
+                return true;
+            if (n < 2 || n % 2 == 0)
+                return false;
+            BigInteger t = n - 1;
+            int s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+            for (int i = 0; i < s + 2; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                byte[] b = new byte[n.ToByteArray().LongLength];
+                BigInteger a;
+                do
+                {
+                    rng.GetBytes(b);
+                    a = new BigInteger(b);
+                }
+                while (a < 2 || a >= n - 2);
+                BigInteger x = BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x != n - 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckProst(int n)
+        {
+            for (int i = 2; i * i < n; i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+
+        private BigInteger nod(BigInteger a, BigInteger b, out BigInteger x, out BigInteger y)
+        {
+            if (a == 0)
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+            BigInteger x1, y1;
+            BigInteger d = nod(b % a, a, out x1, out y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return d;
+        }
+
+    }
+}
